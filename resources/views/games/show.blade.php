8a33af68614b0eec8943a779721b3c6d2abf0a13
Snarie <144214499+Snarie@users.noreply.github.com>
2024-06-06 16:52:19 +0200

    added link to history and removed orderresult

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -16,58 +16,85 @@
                         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
                             Total Cost: {{ $game->totalCost('wholesaler') }}
                         </p>
-                        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                            customerOrderResults = {{ $game->rounds()->sum('customer_orders') }}
-                        </p>
-                        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
 
+                        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('games.history', $game) }}">
+                                {{ __('Show history') }}
+                            </a>
                         </p>
                     @else
-
-                        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                            Current Round: {{ $game->current_round }}
-                        </p>
-                        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                            Max Round: {{ $game->max_round }}
-                        </p>
-                        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                            Total Cost: &euro;{{ $game->totalCost('wholesaler') }}
-                        </p>
-                        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                            Week Cost: &euro;{{$game->currentRound()->cost('wholesaler')}}
-                        </p>
-                        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                            Stock: {{ $game->currentRound()->wholesaler_stock }}
-                        </p>
-                        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                            Backlog: {{$game->currentRound()->wholesaler_backlog}}
-                        </p>
-                        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                            Retailer Order: {{$game->previousOrder()->retailer_order}}
-                        </p>
-                        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                            @if($game->deliveredRound())
-                                Incoming delivery: {{ $game->deliveredRound()->distributor_send}}
-                            @else
-                                Incoming delivery: 400 (not found)
-                            @endif
-                        </p>
-
-                        <!-- Order amount Form-->
-                        <form method="POST" action="{{ route('user_orders.store', $game->slug) }}" class="mt-4">
-                            @csrf
-                            <div class="mb-4">
-                                <label for="order_amount" class="block text-gray-700 dark:text-gray-400">Order Amount</label>
-                                <input type="number" id="order_amount" name="order_amount" required class="mt-1 block w-full">
-                                @error('order_amount')
-                                <span class="text-red-500 text-sm">{{ $message }}</span>
-                                @enderror
+                        <div class="grid grid-cols-2 gap-4">
+                            <!-- First line -->
+                            <div class="bg-gray-200 dark:bg-gray-900 p-4 rounded">
+                                <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                                    Retailer Order: {{ $game->previousOrder()->retailer_order }}
+                                </p>
                             </div>
-                            <x-primary-button class="ms-3">
-                                {{ __('Submit Order') }}
-                            </x-primary-button>
-                            {{--                        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Submit Order</button>--}}
-                        </form>
+                            <div class="bg-gray-200 dark:bg-gray-900 p-4 rounded">
+                                <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                                    Outgoing delivery: {{ $game->currentRound()->wholesaler_send }}
+                                </p>
+                            </div>
+
+                            <!-- Second line -->
+                            <div class="bg-gray-200 dark:bg-gray-900 p-4 rounded">
+                                <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                                    Total Cost: &euro;{{ $game->totalCost('wholesaler') }}
+                                </p>
+                                <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                                    Week Cost: &euro;{{ $game->currentRound()->cost('wholesaler') }}
+                                </p>
+                            </div>
+                            <div class="bg-gray-200 dark:bg-gray-900 p-4 rounded">
+                                <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                                    Stock: {{ $game->currentRound()->wholesaler_stock }}
+                                </p>
+                                @if($game->currentRound()->wholesaler_backlog > 0)
+                                    <p class="mt-1 text-sm text-red-600 dark:text-gray-400">
+                                        Backlog: {{ $game->currentRound()->wholesaler_backlog }}
+                                    </p>
+                                @endif
+                            </div>
+
+                            <!-- Third line -->
+                            <div class="bg-gray-200 dark:bg-gray-900 p-4 rounded">
+                                <!-- Order amount Form-->
+                                <form method="POST" action="{{ route('user_orders.store', $game->slug) }}" class="mt-4">
+                                    @csrf
+                                    <div class="mb-4">
+                                        <label for="order_amount" class="block text-gray-700 dark:text-gray-400">Order Amount</label>
+                                        <input type="number" id="order_amount" name="order_amount" required class="mt-1 block w-full dark:bg-gray-700 dark:bg-gray-200">
+                                        @error('order_amount')
+                                        <span class="text-red-500 text-sm">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                    <x-primary-button class="ms-3">
+                                        {{ __('Submit Order') }}
+                                    </x-primary-button>
+                                </form>
+                            </div>
+                            <div class="bg-gray-200 dark:bg-gray-900 p-4 rounded">
+                                <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                                    @if($game->deliveredRound())
+                                        Incoming delivery: {{ $game->deliveredRound()->distributor_send}}
+                                    @else
+                                        Incoming delivery: 400 (not found)
+                                    @endif
+                                </p>
+                            </div>
+                        </div>
+
+
+                        <!-- Full width -->
+                        <div class="mt-4 bg-gray-200 dark:bg-gray-900 p-4 rounded">
+                            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                                Max Round: {{ $game->max_round }}
+                            </p>
+                            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                                Current Round: {{ $game->current_round }}
+                            </p>
+                        </div>
+
                     @endif
 
                 </div>
